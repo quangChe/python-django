f42787c38b78504e765e941f37c4c315314fd94d
quangChe <quang.n.che@gmail.com>
2019-08-09 16:25:16 -0700

    Added RESTful endpoints for heroes

--- superhumans/urls.py
+++ superhumans/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index'),
+  path('heroes/', views.heroes_list, name='heroes_list'),
+  path('heroes/<int:key>/', views.heroes_detail, name='heroes_detail'),
 ]
