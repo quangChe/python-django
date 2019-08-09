6b0bf96e0386d27040ab81def850c58670ea488f
quangChe <quang.n.che@gmail.com>
2019-08-09 12:39:42 -0700

    Added Heroes and Villains models to the superhumans app and migrated them to heroes database

--- first_api/settings.py
+++ first_api/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'heroes',
+    'superhumans.apps.SuperhumansConfig',
 ]
 
 MIDDLEWARE = [
---
+++ superhumans/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Heroes(models.Model):
+  alias = models.CharField(max_length=100)
+  real_name = models.CharField(max_length=100)
+  superpower = models.CharField(max_length=400)
+  hero_tier = models.IntegerField()
+  nemesis = models.ForeignKey('Villains', on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.alias
+  
+  def can_defeat(self, villain_level):
+    return (11 - villain_level >= self.hero_tier);
+
+class Villains(models.Model):
+  alias = models.CharField(max_length=100)
+  real_name = models.CharField(max_length=100)
+  superpower = models.CharField(max_length=400) 
+  threat_level = models.IntegerField()
+
+  def __str__(self):
+    return self.alias 
+
