4b7727b982643055c6f1b168c007890f1c83decc
quangChe <quang.n.che@gmail.com>
2019-08-09 16:24:59 -0700

    Added JSON responses as views

--- superhumans/views.py
+++ superhumans/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from superhumans.models import Heroes
+from superhumans.serializers import HeroesSerializer
 
-def index(request):
-  return HttpResponse('<em>My App</em>')
+# Remove csrf_exempt for production (bad!)
+@csrf_exempt
+def heroes_list(request):
+  '''
+  List all heroes, or create a new hero
+  '''
+  if request.method == 'GET':
+    heroes = Heroes.objects.all()
+    serializer = HeroesSerializer(heroes, many=True)
+    return JsonResponse(serializer.data, safe=False)
+  # elif request.method == 'POST':
+
+def heroes_detail(request, key):
+  '''
+  Retrieve, update or delete a single hero
+  '''
+  try: 
+    heroes = Heroes.objects.get(pk=key)
+  except Heroes.DoesNotExist:
+    return HttpResponse(status=404)
+
+  if request.method == 'GET':
+    serializer = HeroesSerializer(heroes)
+    return JsonResponse(serializer.data)
