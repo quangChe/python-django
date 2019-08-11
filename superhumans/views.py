469a1e19542385c2fb2d534a525c45ddd11ef2f8
quangChe <quang.n.che@gmail.com>
2019-08-11 11:22:40 -0700

    Added djangorestframework to installed apps and utilized rest_framework response method and api_view decorators inside views

--- superhumans/views.py
+++ superhumans/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from superhumans.models import Heroes
 from superhumans.serializers import HeroesSerializer
 
-# Remove csrf_exempt for production (bad!)
-@csrf_exempt
+@api_view(['GET'])
 def heroes_list(request):
   '''
   List all heroes, or create a new hero
@@ -14,9 +13,9 @@ def heroes_list(request):
   if request.method == 'GET':
     heroes = Heroes.objects.all()
     serializer = HeroesSerializer(heroes, many=True)
-    return JsonResponse(serializer.data, safe=False)
-  # elif request.method == 'POST':
+    return Response(serializer.data)
 
+@api_view(['GET'])
 def heroes_detail(request, key):
   '''
   Retrieve, update or delete a single hero
@@ -24,8 +23,8 @@ def heroes_detail(request, key):
   try: 
     heroes = Heroes.objects.get(pk=key)
   except Heroes.DoesNotExist:
-    return HttpResponse(status=404)
+    return Response(status=status.HTTP_404_NOT_FOUND)
 
   if request.method == 'GET':
     serializer = HeroesSerializer(heroes)
-    return JsonResponse(serializer.data)
+    return Response(serializer.data)
