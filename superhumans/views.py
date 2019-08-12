e22d436e07e52943a53937a620b2fdb2448a7d2b
quangChe <quang.n.che@gmail.com>
2019-08-11 18:24:28 -0700

    Added a post method for Heroes

--- superhumans/views.py
+++ superhumans/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from superhumans.models import Heroes
 from superhumans.serializers import HeroesSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def heroes_list(request):
   '''
   List all heroes, or create a new hero
@@ -14,6 +15,15 @@ def heroes_list(request):
     heroes = Heroes.objects.all()
     serializer = HeroesSerializer(heroes, many=True)
     return Response(serializer.data)
+
+  elif request.method == 'POST':
+    serializer = HeroesSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 @api_view(['GET'])
 def heroes_detail(request, key):
