2a8de39a9ae5775f29c260c9b138db6f4cc096e3
quangChe <quang.n.che@gmail.com>
2019-08-14 12:41:41 -0700

    Minimialized HeroesList and HeroesDetail class-based views using specific generics, allowing for minimal code with maximum functionality

--- superhumans/views.py
+++ superhumans/views.py
@@ -1,40 +1,17 @@
 from superhumans.models import Heroes, Villains
 from superhumans.serializers import HeroesSerializer, VillainsSerializer
-from rest_framework import mixins, generics
+from rest_framework import generics
 
-class HeroesList(mixins.ListModelMixin, 
-                 mixins.CreateModelMixin, 
-                 generics.GenericAPIView):
+class HeroesList(generics.ListCreateAPIView):
   """
   List all heroes, or create a new hero.
   """
-
   queryset = Heroes.objects.all()
   serializer_class = HeroesSerializer
 
-  def get(self, request, *args, **kwargs):
-    return self.list(request, *args, **kwargs)
-
-  def post(self, request, *args, **kwargs):
-    return self.create(request, *args, **kwargs)
-
-class HeroesDetail(mixins.RetrieveModelMixin,
-                   mixins.UpdateModelMixin,
-                   mixins.DestroyModelMixin,
-                   generics.GenericAPIView):
+class HeroesDetail(generics.RetrieveUpdateDestroyAPIView):
   """
   Retrieve, update and delete
   """
-
   queryset = Heroes.objects.all()
   serializer_class = HeroesSerializer
-
-  def get(self, request, *args, **kwargs):
-    return self.retrieve(request, *args, **kwargs)
-
-  def put(self, request, *args, **kwargs):
-    return self.update(request, *args, **kwargs)
-
-  def delete(self, request, *args, **kwargs):
-    return self.destroy(request, *args, **kwargs)
-  
