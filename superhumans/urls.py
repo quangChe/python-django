4d85253b58a3821e641ce6e0cb99f372db0272d5
quangChe <quang.n.che@gmail.com>
2019-08-14 13:48:43 -0700

    Added a VillainsList and VillainsDetail view

--- superhumans/urls.py
+++ superhumans/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
   path('heroes/', views.HeroesList.as_view(), name='heroes_list'),
   path('heroes/<int:pk>/', views.HeroesDetail.as_view(), name='heroes_detail'),
+  path('villains/', views.VillainsList.as_view(), name='villains_list'),
+  path('villains/<int:pk>/', views.VillainsDetail.as_view(), name='villains_detail'),
 ]
--- superhumans/views.py
+++ superhumans/views.py
@@ -4,14 +4,28 @@ from rest_framework import generics
 
 class HeroesList(generics.ListCreateAPIView):
   """
-  List all heroes, or create a new hero.
+  List all heroes, or create a new hero
   """
   queryset = Heroes.objects.all()
   serializer_class = HeroesSerializer
 
 class HeroesDetail(generics.RetrieveUpdateDestroyAPIView):
   """
-  Retrieve, update and delete
+  Retrieve, update and delete a specific hero
   """
   queryset = Heroes.objects.all()
   serializer_class = HeroesSerializer
+
+class VillainsList(generics.ListCreateAPIView):
+  """
+  List all villains or create a new villain
+  """
+  queryset = Villains.objects.all()
+  serializer_class = VillainsSerializer
+
+class VillainsDetail(generics.RetrieveUpdateDestroyAPIView):
+  """
+  Retrieve, update and delete a specific villain
+  """
+  queryset = Villains.objects.all()
+  serializer_class = VillainsSerializer
